dcc31b0fa89c23d09e9460c3f19c3f042cdcc1d2
weiguang1019 <639075562@qq.com>
2023-01-25 00:01:45 +0800

    init yml

---
+++ mycloud/cloud-gateway-gateway9527/src/main/java/com/atguigu/springcloud/config/GateWayConfig.java
@@ -0,0 +1,26 @@
+package com.atguigu.springcloud.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class GateWayConfig {
+
+    @Bean
+    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+        RouteLocatorBuilder.Builder routes = builder.routes();
+        routes.route("path_route_atguigu",r ->
+                r.path("/news").uri("https://news.baidu.com/")).build();
+        return routes.build();
+    }
+
+    @Bean
+    public RouteLocator customRouteLocator2(RouteLocatorBuilder builder) {
+        RouteLocatorBuilder.Builder routes = builder.routes();
+        routes.route("path_route_atguigu2",r ->
+                r.path("/tieba").uri("https://tieba.baidu.com/index.html")).build();
+        return routes.build();
+    }
+}
